8e2d659f7866d6460996a706e2c24c79b3ce3cd0
kihwn <kihwn98@gmail.com>
2023-12-11 15:59:52 +0900

    feat: 기본 기능 구현 중

--- src/main/java/kuit/springbasic/web/controller/HomeController.java
+++ src/main/java/kuit/springbasic/web/controller/HomeController.java
@@ -1,19 +1,42 @@
 package kuit.springbasic.web.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import kuit.springbasic.web.dao.QuestionDao;
+import kuit.springbasic.web.domain.Question;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.sql.SQLException;
+import java.util.List;
 
 @Slf4j
+@Controller
+@RequiredArgsConstructor
 public class HomeController {
 
+    private final QuestionDao questionDao;
+
+
+
     @RequestMapping("/")
-    public String showHome(Model model) {
+    public ModelAndView showHome (HttpServletRequest request, HttpServletResponse response) throws SQLException {
         log.info("HomeController.showHome");
 
-        // 코드 추가 필요
+        ModelAndView modelAndView = new ModelAndView("/home");
 
-        return "home";
+        List<Question> questions = questionDao.findAll();
+        modelAndView.addObject("questions",questions);
+
+        //
+
+        return modelAndView;
     }
 
 }
